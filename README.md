0c7e33b8fe307fb9c05ddfa8d67bc076666bb48e
Tony Liou <ltony1024@yahoo.com.tw>
2020-05-31 00:49:02 +0800

    First init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Youtube Upload Video
+
+A web page for Google OAuth 2.0 
---
+++ docs/assets/js/main.js
@@ -0,0 +1,26 @@
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1141px',  '1680px' ],
+			large:    [ '981px',   '1140px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '321px',   '480px'  ],
+			xxsmall:  [ null,      '320px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly').scrolly();
+
+})(jQuery);
